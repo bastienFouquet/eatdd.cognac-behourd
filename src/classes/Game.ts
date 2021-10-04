381ad4d68e25d757d89fdc4a21c1c904d5858109
kctus9 <kctusx@gmail.com>
2021-10-04 17:33:33 +0200

    create game and balance teams

---
+++ src/classes/Game.ts
@@ -0,0 +1,45 @@
+import {Person} from "./Person";
+import {Team} from "./Team";
+
+export class Game {
+    public teamA: Team;
+    public teamB: Team;
+
+    constructor(members: Person[]) {
+        this.teamA = new Team();
+        this.teamB = new Team();
+
+        this.teamA.members.push(members[members.length-1]);
+        members.pop();
+        this.teamB.members.push(members[members.length-1]);
+        members.pop();
+
+        while (members.length>=1){
+
+            if(this.teamA.members.length > this.teamB.members.length){
+                this.teamB.members.push(members[members.length-1]);
+
+            }else if (this.teamA.members.length < this.teamB.members.length){
+                this.teamA.members.push(members[members.length-1]);
+
+            }else if (this.teamA.members.length === this.teamB.members.length){
+
+                if (this.teamA.getAvarageWeightCategory() > this.teamB.getAvarageWeightCategory()){
+                    this.teamB.members.push(members[members.length-1]);
+
+                }else if (this.teamA.getAvarageWeightCategory() < this.teamB.getAvarageWeightCategory()){
+                    this.teamA.members.push(members[members.length-1]);
+
+                }else if (this.teamA.getAvarageWeightCategory() === this.teamB.getAvarageWeightCategory()){
+                    if(this.teamA.getAverageSeniority() > this.teamB.getAverageSeniority()){
+                        this.teamB.members.push(members[members.length-1]);
+                    }else{
+                        this.teamA.members.push(members[members.length-1]);
+                    }
+                }
+            }
+            members.pop();
+        }
+    }
+
+}
