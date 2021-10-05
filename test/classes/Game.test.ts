35675df0de71c818c32bbb9123717bcad611ea54
kctus9 <kctusx@gmail.com>
2021-10-05 16:51:47 +0200

    Game.test + Person.test

--- test/classes/Game.test.ts
+++ test/classes/Game.test.ts
@@ -1,51 +1,100 @@
 import {Person} from "../../src/classes/Person";
 import {Game} from "../../src/classes/Game";
 
-describe('Game', () => {
-  describe('New game', () => {
-    it('should initialize a game with two balanced teams', () => {
-      const person1: Person = new Person({
-          firstname: 'Paul',
-          lastname: 'Gros',
-          weight: 134,
-          weapon: 'Hache 2M',
-          yearRegistration: 2006
-        }
-      );
+let game: Game;
+beforeAll(() => {
+    game = new Game(buildingMembers());
+});
 
-      const person2: Person = new Person({
+function randomWeight() {
+    return Math.random() * (150 - 45) + 45;
+}
+
+function randomYearRegistration() {
+    return Math.random() * (2021 - 1980) + 1980;
+}
+
+function buildingMembers() {
+    const person1: Person = new Person({
+        firstname: 'Paul',
+        lastname: 'Gros',
+        weight: randomWeight(),
+        weapon: 'Hache 2M',
+        yearRegistration: randomYearRegistration()
+    });
+    const person2: Person = new Person({
         firstname: 'Louis',
         lastname: 'Blanc',
-        weight: 47,
+        weight: randomWeight(),
         weapon: 'Dagues',
-        yearRegistration: 2020
-      });
-
-      const person3: Person = new Person({
+        yearRegistration: randomYearRegistration()
+    });
+    const person3: Person = new Person({
         firstname: 'Jean-Michel',
         lastname: 'Giraud',
-        weight: 79,
+        weight: randomWeight(),
         weapon: 'Sabre',
-        yearRegistration: 1987
-      });
-
-      const person4: Person = new Person({
+        yearRegistration: randomYearRegistration()
+    });
+    const person4: Person = new Person({
         firstname: 'Théophile',
         lastname: 'Paris',
-        weight: 102,
+        weight: randomWeight(),
         weapon: 'Hallebarde',
-        yearRegistration: 2003
-      });
+        yearRegistration: randomYearRegistration()
+    });
+    const person5: Person = new Person({
+        firstname: 'Théophile',
+        lastname: 'Paris',
+        weight: randomWeight(),
+        weapon: 'Hallebarde',
+        yearRegistration: randomYearRegistration()
+    });
+    const person6: Person = new Person({
+        firstname: 'Théophile',
+        lastname: 'Paris',
+        weight: randomWeight(),
+        weapon: 'Hallebarde',
+        yearRegistration: randomYearRegistration()
+    });
+    const person7: Person = new Person({
+        firstname: 'Théophile',
+        lastname: 'Paris',
+        weight: randomWeight(),
+        weapon: 'Hallebarde',
+        yearRegistration: randomYearRegistration()
+    });
+    const person8: Person = new Person({
+        firstname: 'Théophile',
+        lastname: 'Paris',
+        weight: randomWeight(),
+        weapon: 'Hallebarde',
+        yearRegistration: randomYearRegistration()
+    });
+    return [person1, person2, person3, person4, person5, person6, person7, person8];
+}
 
-      const members = [person2, person4, person1, person3];
 
-      const game: Game = new Game(members);
+describe('Game', () => {
+    describe('New game with balanced teams', () => {
+        it('teams have almost same average weight category', () => {
+            expect(game.teamA.getAverageWeightCategory().id).toBeCloseTo(game.teamA.getAverageWeightCategory().id, 0);
+        })
+        it('teams have same number of members', () => {
+            const teamALength = game.teamA.members.length;
+            const teamBLength = game.teamB.members.length;
+            expect(teamALength).toEqual(teamBLength);
+        })
+        it('teams take all members', () => {
+            const teamALength = game.teamA.members.length;
+            const teamBLength = game.teamB.members.length;
+            expect(buildingMembers().length).toEqual(teamALength + teamBLength);
+        })
+        it('teams have almost same average seniority (+/- 5 years)', () => {
+            console.log(game.teamA.getAverageSeniority());
+            console.log(game.teamB.getAverageSeniority());
+            expect(game.teamA.getAverageSeniority()).toBeCloseTo(game.teamB.getAverageSeniority(), -1);
 
-      expect(game.teamA.members.length === game.teamB.members.length).toBe(true);
-      expect(game.teamA.members.length + game.teamB.members.length).toEqual(4);
-      if (game.teamA.members.length === game.teamB.members.length) {
-        expect(game.teamA.getAverageWeightCategory()).toBe(game.teamB.getAverageWeightCategory());
-      }
+        })
     })
-  })
 })
--- test/classes/Person.test.ts
+++ test/classes/Person.test.ts
@@ -1,28 +1,28 @@
 import {Person} from '../../src/classes/Person';
+import {WeightCategoryService} from "../../src/services/WeightCategoryService";
 
 describe('Person', () => {
-  describe('New person', () => {
-    it('new person added to game', () => {
-      const person: Person = new Person({
-        firstname: 'Jean',
-        lastname: 'DUPONT',
-        weightCategory:
-          {id: 1, label: 'Mouche', max: 52, min: 57},
-        weapon: 'Hallebarde'
-      });
-      expect(person.weight).toBe(54.5);
-      expect(person.armor).toBe('Gambison');
+    describe('New person', () => {
+        it('new person added to game', () => {
+            const person: Person = new Person({
+                firstname: 'Jean',
+                lastname: 'DUPONT',
+                weightCategory: WeightCategoryService.getInstance().weightCategories.find(el => el.id === 2),
+                weapon: 'Hallebarde'
+            });
+            expect(person.weight).toBe(54.5);
+            expect(person.armor).toBe('Gambison');
+        });
+        it('new person already members', () => {
+            const person: Person = new Person({
+                firstname: 'Jean',
+                lastname: 'DUPONT',
+                weight: 56,
+                weapon: 'Hallebarde',
+                armor: 'Mailles',
+                yearRegistration: 2000
+            });
+            expect(person.weight).toBe(56);
+        });
     });
-    it('new person already members', () => {
-      const person: Person = new Person({
-        firstname: 'Jean',
-        lastname: 'DUPONT',
-        weight: 56,
-        weapon: 'Hallebarde',
-        armor: 'Mailles',
-        yearRegistration: 2000
-      });
-      expect(person.weight).toBe(56);
-    });
-  });
 });
