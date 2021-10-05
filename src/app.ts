d7cf7483452fe9ac44aae8e24d6e515806bee286
Louis Bernardeau <lbernardeau@la-joliverie.com>
2021-10-05 10:39:47 +0200

    ajout demande console path

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import {WeightCategoryService} from './services/WeightCategoryService';
+import { importXlsx } from "./import-xlsx";
+import * as readline from 'readline';
 
-function run() {
-  console.log('App started');
-  WeightCategoryService.getInstance();
+async function run() {
+    console.log('App started');
+    WeightCategoryService.getInstance();
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Veuillez indiquer le path du fichier xlsx : ', (answer) => {
+        importXlsx(answer);
+        rl.close();
+    });
 }
 
 run();
+
+// ./files/Exemple.xlsx
--- src/import-xlsx.ts
+++ src/import-xlsx.ts
@@ -1,16 +1,26 @@
-const xlsx = require('xlsx');
+import { Person } from "./classes/Person";
+
+
+export function importXlsx(path: string): Person[] {
+    const xlsx = require('xlsx');
+
+/*export function importXlsx(path: string) {
+    const xlsx = require('xlsx');*/
+
 
 const filePath = process.argv.slice(2)[0];
-const workbook = xlsx.readFile(filePath);
+const workbook = xlsx.readFile(path);
 const worksheet = workbook.Sheets[workbook.SheetNames[0]];
 
 let posts = [];
+const persons: Person[] = [];
 let post: any = {};
 
 for (let cell in worksheet) {
     const cellAsString:any = cell.toString();
 
     if (cellAsString[1] !== 'r' && cellAsString[1] !== 'm' && cellAsString[1] > 1) {
+
         if (cellAsString[0] === 'A') {
             post.name = worksheet[cell].v;
         }
@@ -29,13 +39,18 @@ for (let cell in worksheet) {
         if (cellAsString[0] === 'F') {
             post.armor = worksheet[cell].v;
             posts.push(post);
-            post = {};
+            const person: Person = Object.assign(Person, {firstname: post.firstname, lastname: post.name, yearRegistration: post.year_register, weight: post.weigth, weightCategory: {id: 1, label: 'Mouche', max: 52, min: 57}, weapon: post.weapon, armor: post.armor });
+            persons.push(person)
+
+            console.log(person)
         }
     }
 }
 
-console.log(posts);
 
+//console.log(posts);
+return persons
 
+}
 
 //node .\src\import-xlsx.js .\files\Exemple.xlsx <-- pour lancer
